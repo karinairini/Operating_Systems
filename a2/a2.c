227b2d9e37b6427eee21c2e0637bec4f4c63c5db
Irini Karina <irini.karina2@gmail.com>
2023-05-04 21:31:08 +0300

    commit

--- a2/a2.c
+++ a2/a2.c
@@ -20,6 +20,10 @@ sem_t semThread4;
 sem_t *semaphore1;
 sem_t *semaphore2;
 
+int runningThreads = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
 void *thread_function_P7(void *arg)
 {
 	TH_STRUCT *s = (TH_STRUCT *)arg;
@@ -55,8 +59,24 @@ void *thread_function_P7(void *arg)
 void *thread_function_P2(void *arg)
 {
 	TH_STRUCT *s = (TH_STRUCT *)arg;
+	pthread_mutex_lock(&lock);
+	
+	while(runningThreads >= 6)
+	{
+		pthread_cond_wait(&cond, &lock);
+	}
+	++runningThreads;
+	
+	pthread_mutex_unlock(&lock);
+	
 	info(BEGIN, s->processNumber, s->threadNumber);
 	info(END, s->processNumber, s->threadNumber);
+	
+	pthread_mutex_lock(&lock);
+	--runningThreads;
+	pthread_mutex_unlock(&lock);
+	
+	pthread_cond_signal(&cond);
 	return NULL;
 }
 
@@ -85,135 +105,135 @@ void *thread_function_P4(void *arg)
 
 int main()
 {
-    init();
-    info(BEGIN, 1, 0);
-    sem_unlink("/semaphore1");
-    semaphore1 = sem_open("/semaphore1", O_CREAT, 0644, 0); 
-    if(semaphore1 == NULL) 
-    {
-	perror("Could not aquire the semaphore");
-	return -2;
-    }
-    sem_unlink("/semaphore2");
-    semaphore2 = sem_open("/semaphore2", O_CREAT, 0644, 0); 
-    if(semaphore2 == NULL) 
-    {
-	perror("Could not aquire the semaphore");
-	return -2;
-    }
-    pid_t p2 = -1, p3 = -1, p4 = -1, p5 = -1, p6 = -1, p7 = -1, p8 = -1; 
-    p2 = fork();
-    if(p2 == 0)
-    {
-    	info(BEGIN, 2, 0);
-    	pthread_t tids[48];
-    	TH_STRUCT params[48];
-    	int i;
-    	for(i = 0; i < 48; i++)
+	init();
+    	info(BEGIN, 1, 0);
+    	sem_unlink("/semaphore1");
+    	semaphore1 = sem_open("/semaphore1", O_CREAT, 0644, 0); 
+    	if(semaphore1 == NULL) 
     	{
-    		params[i].threadNumber = i + 1;
-    		params[i].processNumber = 2;
-    		pthread_create(&tids[i], NULL, thread_function_P2, &params[i]);
+		perror("Could not aquire the semaphore");
+		return -2;
     	}
-    	for(i = 0; i < 48; i++)
+    	sem_unlink("/semaphore2");
+    	semaphore2 = sem_open("/semaphore2", O_CREAT, 0644, 0); 
+    	if(semaphore2 == NULL) 
     	{
-    		pthread_join(tids[i], NULL);
+		perror("Could not aquire the semaphore");
+		return -2;
     	}
-    	p3 = fork();
-    	if(p3 == 0)
+    	pid_t p2 = -1, p3 = -1, p4 = -1, p5 = -1, p6 = -1, p7 = -1, p8 = -1; 
+    	p2 = fork();
+    	if(p2 == 0)
     	{
-    		info(BEGIN, 3, 0);
-    		p7 = fork();
-    		if(p7 == 0)
+    		info(BEGIN, 2, 0);
+    		pthread_t tids[48];
+    		TH_STRUCT params[48];
+    		int i;
+    		for(i = 0; i < 48; i++)
     		{
-    			info(BEGIN, 7, 0);
-    			pthread_t tids[5];
-    			TH_STRUCT params[5];
-    			int i;
-    			for(i = 0; i < 5; i++)
+    			params[i].threadNumber = i + 1;
+    			params[i].processNumber = 2;
+    			pthread_create(&tids[i], NULL, thread_function_P2, &params[i]);
+    		}
+    		for(i = 0; i < 48; i++)
+    		{
+    			pthread_join(tids[i], NULL);
+    		}
+    		p3 = fork();
+    		if(p3 == 0)
+    		{
+    			info(BEGIN, 3, 0);
+    			p7 = fork();
+    			if(p7 == 0)
     			{
-    				params[i].threadNumber = i + 1;
-    				params[i].processNumber = 7;
-    				pthread_create(&tids[i], NULL, thread_function_P7, &params[i]);
-    			}
-    			for(i = 0; i < 5; i++)
+    				info(BEGIN, 7, 0);
+    				pthread_t tids[5];
+    				TH_STRUCT params[5];
+    				int i;
+    				for(i = 0; i < 5; i++)
+    				{
+    					params[i].threadNumber = i + 1;
+    					params[i].processNumber = 7;
+    					pthread_create(&tids[i], NULL, thread_function_P7, &params[i]);
+    				}
+    				for(i = 0; i < 5; i++)
+    				{
+    					pthread_join(tids[i], NULL);
+    				}
+    				info(END, 7, 0);
+    				exit(7);
+    			}	
+    			else	
     			{
-    				pthread_join(tids[i], NULL);
+    				waitpid(p7, NULL, 0);
+    				info(END, 3, 0);
+    				exit(3);
     			}
-    			info(END, 7, 0);
-    			exit(7);
     		}
     		else
     		{
-    			waitpid(p7, NULL, 0);
-    			info(END, 3, 0);
-    			exit(3);
+    			waitpid(p3, NULL, 0);
+    			info(END, 2, 0);
+    			exit(2);
     		}
     	}
-    	else
+    	p4 = fork();
+    	if(p4 == 0)
     	{
-    		waitpid(p3, NULL, 0);
-    		info(END, 2, 0);
-    		exit(2);
-    	}
-    }
-    p4 = fork();
-    if(p4 == 0)
-    {
-    	info(BEGIN, 4, 0);
-    	pthread_t tids[5];
-    	TH_STRUCT params[5];
-    	int i;
-    	semaphore1 = sem_open("/semaphore1", 0); 
-    	semaphore2 = sem_open("/semaphore2", 0); 
-    	for(i = 0; i < 5; i++)
-    	{
-    		params[i].threadNumber = i + 1;
-    		params[i].processNumber = 4;
-    		pthread_create(&tids[i], NULL, thread_function_P4, &params[i]);
-    	}
-    	for(i = 0; i < 5; i++)
-    	{
-		pthread_join(tids[i], NULL);
-    	}
-    	sem_close(semaphore1);
-    	sem_close(semaphore2);
-    	info(END, 4, 0);
-    	exit(4);
-    }	
-    p5 = fork();
-    if(p5 == 0)
-    {
-    	info(BEGIN, 5, 0);
-    	p6 = fork();
-    	if(p6 == 0)
-    	{
-    		info(BEGIN, 6, 0);
-    		info(END, 6, 0);
-    		exit(6);
-    	}
-    	else
-    	{
-    		waitpid(p6, NULL, 0);
-    		p8 = fork();
-    		if(p8 == 0)
+    		info(BEGIN, 4, 0);
+    		pthread_t tids[5];
+    		TH_STRUCT params[5];
+    		int i;
+    		semaphore1 = sem_open("/semaphore1", 0); 
+    		semaphore2 = sem_open("/semaphore2", 0); 
+    		for(i = 0; i < 5; i++)
     		{
-    			info(BEGIN, 8, 0);
-    			info(END, 8, 0);
-    			exit(8);
+    			params[i].threadNumber = i + 1;
+    			params[i].processNumber = 4;
+    			pthread_create(&tids[i], NULL, thread_function_P4, &params[i]);
+    		}	
+    		for(i = 0; i < 5; i++)
+    		{
+			pthread_join(tids[i], NULL);
+    		}
+    		sem_close(semaphore1);
+    		sem_close(semaphore2);
+    		info(END, 4, 0);
+    		exit(4);
+    	}	
+    	p5 = fork();
+    	if(p5 == 0)
+    	{
+    		info(BEGIN, 5, 0);
+    		p6 = fork();
+    		if(p6 == 0)
+    		{
+    			info(BEGIN, 6, 0);
+    			info(END, 6, 0);
+    			exit(6);
     		}
     		else
     		{
-    			waitpid(p8, NULL, 0);
-    			info(END, 5, 0);
-    			exit(5);
+    			waitpid(p6, NULL, 0);
+    			p8 = fork();
+    			if(p8 == 0)
+    			{
+    				info(BEGIN, 8, 0);
+    				info(END, 8, 0);
+    				exit(8);
+    			}
+    			else
+    			{
+    				waitpid(p8, NULL, 0);
+    				info(END, 5, 0);
+    				exit(5);
+    			}
     		}
     	}
-    }
-   waitpid(p2, NULL, 0);
-   waitpid(p4, NULL, 0); 		
-   waitpid(p5, NULL, 0);
-   info(END, 1, 0);
-   exit(1);
-   return 0;
+   	waitpid(p2, NULL, 0);
+   	waitpid(p4, NULL, 0); 		
+   	waitpid(p5, NULL, 0);
+   	info(END, 1, 0);
+   	exit(1);
+   	return 0;
 }
